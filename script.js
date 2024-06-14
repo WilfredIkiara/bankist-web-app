63497675bf91cc80b82289a1e0cc05fe70cbcc98
WILFRED IKIARA <133862973+WilfredIkiara@users.noreply.github.com>
2024-06-14 22:50:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -109,8 +109,8 @@ const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = "";
 
   const movs = sort
-    ? movements.slice().sort((a, b) => a - b)
-    : movements;
+    ? acc.movements.slice().sort((a, b) => a - b)
+    : acc.movements;
 
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? "deposit" : "withdrawal";
